32a386e80b0a2b92b815bd8599c24138c5425602
Brandon Werner <47579647+BC-Werner@users.noreply.github.com>
2021-12-06 21:27:41 -0500

    Removed unused includes

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,19 @@
  */
 
 #include <iostream>
- //#include <vector>
- //#include <string>
- //#include <algorithm>
 
 #include "../lib/Strategy/include/Strategy/Client.hpp"
 #include "../lib/Strategy/include/Strategy/Strategy.hpp"
 #include "../lib/Strategy/include/Strategy/ConcreteStrategy.hpp"
 
-//using namespace std;
 
 int main()
 {
 
+    // Create the client with a concrete strategy and execute the strategy
     Client* client = new Client(new ConcreteStrategy);
     client->executeStrategy();
     delete client;
-
 
     return 0;
 }
